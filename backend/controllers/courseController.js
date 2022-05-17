b1c9a431489c9b270bfa90640e46821dcb54f283
William <wgumboc@gmail.com>
2022-05-17 15:07:35 -0700

    Added async handler

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,39 +1,41 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get courses
 // @route GET /api/goals
 // @access Private
 
-const getCourses = (req, res) => {
+const getCourses = asyncHandler(async(req, res) => {
     res.status(200).json({message: 'Get courses'})
-}
+})
 
 // @desc Set course
 // @route POST /api/goals
 // @access Private
 
-const setCourse = (req, res) => {
+const setCourse = asyncHandler(async(req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json({message: 'Set course'})
-}
+})
 
 // @desc Update course
 // @route PUT /api/goals/id
 // @access Private
 
-const updateCourse = (req, res) => {
+const updateCourse = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Update course ${req.params.id}`})
-}
+})
 
 // @desc Delete course
 // @route DELETE /api/goals/id
 // @access Private
 
-const deleteCourse = (req, res) => {
+const deleteCourse = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Delete course ${req.params.id}`})
-}
+})
 
 
 module.exports = {
