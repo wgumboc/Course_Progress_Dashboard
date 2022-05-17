b975ac5ba71e4beebda8e00afb23afc2b2c987a1
William <wgumboc@gmail.com>
2022-05-17 14:53:36 -0700

    added error and exception handling

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -11,6 +11,11 @@ const getCourses = (req, res) => {
 // @access Private
 
 const setCourse = (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
     res.status(200).json({message: 'Set course'})
 }
 
